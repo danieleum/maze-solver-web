32c657e6afd4616cbbccbf71622cd44922f14435
Daniel Eum <danieleum@daniels-mbp.lan>
2021-07-13 08:51:47 -0700

    First commit

---
+++ src/board/MazeSolver.jsx
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+
+import Square from '../components/Square';
+
+import './MazeSolver.css';
+
+export default class MazeSolver extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: [],
+    };
+  }
+
+  componentDidMount() {
+    const squares = [];
+    for (let row = 0; row < 20; row++) {
+      const current = [];
+      for (let col = 0; col < 20; col++) {
+        const newSquare = {
+          row: row,
+          col: col,
+          wall: false,
+          start: row === 0 && col === 0,
+          end: row === 19 && col === 19
+        }
+        current.push(newSquare);
+      }
+      squares.push(current);
+    }
+    this.setState({squares});
+  }
+  
+  handleClick(row, col) {
+    let newBoard = createWall(this.state.squares, row, col);
+    this.setState({squares: newBoard});
+  }
+
+
+  render() {
+    const {squares} = this.state;
+    console.log(squares)
+
+    return (
+      <div className="board">
+        {squares.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex}>
+              {row.map((square, squareIndex) => {
+                const {start, end, wall} = square;
+                return (
+                  <Square col={squareIndex} row={rowIndex} key={squareIndex} start={start} end={end} wall={wall} onClick={(row, col) => this.handleClick(row, col)}></Square>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+function createWall (squares, row, col) {
+  let newBoard = squares.slice();
+  let current = newBoard[row][col];
+  /*
+  let wallSquare = {
+    ...current,
+    wall: !current.wall
+  }
+  */
+  current.wall = !current.wall;
+  newBoard[row][col] = current;
+  return newBoard;
+}
+
---
+++ src/components/Square.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import './Square.css';
+
+export default class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    let {col, row, start, end, wall, onClick} = this.props;
+    let name;
+    if (start) {
+      name = 'square starting';
+    } else if (end) {
+      name = 'square ending';
+    } else if (wall) {
+      name = 'square wall';
+    } else {
+      name = 'square';
+    }
+
+    return <div onClick={() => onClick(row, col)} className={`${name}`}></div>;
+  }
+}
